c612e141d76330ff98991d23d6da6dc1dafe95fd
María Belén Calvo <mariabe.calvo@gmail.com>
2020-09-08 17:21:24 -0400

    updated current weather icon

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ function updateCurrentDate(date) {
 // search and update to city searched
 
 function displayLocationWeather(response) {
+  let iconElement = document.querySelector("#weather-icon");
+  let iconApi = `${response.data.weather[0].icon}`;
+
   document.querySelector("#city-displayed").innerHTML = response.data.name;
   document.querySelector(
     "#country-displayed"
@@ -35,6 +38,53 @@ function displayLocationWeather(response) {
   );
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  if (iconApi === "01d") {
+    iconElement.setAttribute("class", `fas fa-sun`);
+  } else {
+    if (iconApi === "02d") {
+      iconElement.setAttribute("class", `fas fa-cloud-sun`);
+    } else {
+      if (iconApi === "01n") {
+        iconElement.setAttribute("class", `fas fa-moon`);
+      } else {
+        if (iconApi === " 02n") {
+          iconElement.setAttribute("class", `fas fa-cloud-moon`);
+        } else {
+          if (iconApi === "11d") {
+            iconElement.setAttribute("class", `fas fa-bolt`);
+          } else {
+            if (
+              iconApi === "03d" ||
+              iconApi === "03n" ||
+              iconApi === "04d" ||
+              iconApi === "04n"
+            ) {
+              iconElement.setAttribute("class", `fas fa-cloud`);
+            } else {
+              if (iconApi === "09d") {
+                iconElement.setAttribute("class", `fas fa-cloud-rain`);
+              } else {
+                if (iconApi === "10d") {
+                  iconElement.setAttribute(
+                    "class",
+                    `fas fa-cloud-showers-heavy`
+                  );
+                } else {
+                  if (iconApi === "50d") {
+                    iconElement.setAttribute("class", `fas fa-stream`);
+                  } else {
+                    if (iconApi === "13d") {
+                      iconElement.setAttribute("class", `far fa-snowflake`);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 function searchCity(city) {
@@ -77,7 +127,7 @@ function changeToFarenheit(event) {
 // VARIABLES | display current date and time
 let todayLine = document.querySelector("#right-now");
 let currentDay = new Date();
-todayLine.innerHTML = `Last update: ${updateCurrentDate(currentDay)}`;
+todayLine.innerHTML = `Last updated: ${updateCurrentDate(currentDay)}`;
 
 // VARIABLES | search and update to city searched
 
